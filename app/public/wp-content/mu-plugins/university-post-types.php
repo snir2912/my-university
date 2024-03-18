1ea2b2db8635557c8666b6c41c1ab1ed06fa889f
snir2912 <you@example.com>
2024-03-18 22:29:00 +0200

    create custom fields

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -3,7 +3,7 @@
 function university_post_types() {
   register_post_type('event', array(
     'show_in_rest' => true,
-    'supports' => array('title', 'editor', 'excerpt'),
+    'supports' => array('title', 'editor', 'excerpt',),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
