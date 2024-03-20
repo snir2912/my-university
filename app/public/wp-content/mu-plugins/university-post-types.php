fd669b18f59ba08059fd698972eb9d7b8e83a9b8
snir2912 <you@example.com>
2024-03-20 21:56:32 +0200

    build program post type

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -17,6 +17,23 @@ function university_post_types() {
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
-}
 
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+}
 add_action('init', 'university_post_types');
