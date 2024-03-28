11986f008b574f7a2b22b3a41d3bdb358c2152ab
snir2912 <you@example.com>
2024-03-28 07:31:17 +0200

    use my page banner component

--- app/public/wp-content/themes/university2024/front-page.php
+++ app/public/wp-content/themes/university2024/front-page.php
@@ -6,7 +6,7 @@
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="<?php get_post_type_archive_link('ptogram'); ?> " class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
 
@@ -33,25 +33,9 @@
             ));
 
             while($homePageEvents->have_posts()){
-              $homePageEvents->the_post(); ?>
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month"><?php 
-                  $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('M');
-                  ?></span>
-                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h5>
-                  <p><?php if(has_excerpt()){
-                      echo get_the_excerpt();
-                      }else{
-                        echo wp_trim_words(get_the_content(),20 );
-                     }?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                </div>
-              </div>
-            <?php }
+              $homePageEvents->the_post();
+              get_template_part('template-parts/content', 'event');
+            }
           ?>
           
 
